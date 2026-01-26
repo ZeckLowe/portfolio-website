2e6f97edf993a4549a6ae0e53e60827d145774d5
ZeckLowe <czechlouie06@gmail.com>
2026-01-26 10:28:35 +0800

    init

---
+++ src/constants/data.jsx
@@ -0,0 +1,49 @@
+import { Github, Linkedin, Twitter, Youtube } from "lucide-react";
+
+export const personalInfo = {
+  name: "Czech Louie Sadiang-abay",
+  role: "Data Analyst",
+  bio: "Hi, I'm Czech. I'm a Data Analyst focused on building fast, efficient, and detail-oriented data experiences. My work blends technical precision with a design-driven mindset to create websites that not only work well but feel great.",
+  email: "czechlouie06@gmail.com",
+  locaiton: "Cebu, Philippines",
+};
+
+export const projects = [1, 2, 3, 4, 5, 6];
+
+export const socialLinks = [
+  {
+    name: "Github",
+    icon: Github,
+    link: "#",
+    color: "hover:text-white",
+  },
+  {
+    name: "LinkedIn",
+    icon: Linkedin,
+    link: "#",
+    color: "hover:text-blue-400",
+  },
+];
+
+export const experience = [
+  {
+    title: "Intern",
+    company: "AMCS Group",
+    period: "January 2025 - May 2025",
+    desc: "QA",
+    color: "violet",
+  },
+];
+
+export const tools = ["VS Code - Coding"];
+
+export const goals = ["Learn to create an LLM"];
+
+export const achievements = [
+  {
+    title: "Projects",
+    value: "100",
+    desc: "developed",
+    color: "yellow",
+  },
+];
